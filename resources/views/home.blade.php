3d92caf733d9a4c875dc99b4ff65cb5f24b475e7
wofakouadio <kouamekouadiofrancisbennett@gmail.com>
2022-02-28 10:56:13 +0000

    Initial commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-3 p-5">
+            <img src="/svg/code.svg" alt="" class="rounded-circle">
+        </div>
+        <div class="col-9 pt-5">
+            <div><h1>Laravel First Project</h1></div>
+            <div class="d-flex">
+                <div class="pr-5"><strong>999,999k</strong> posts</div>
+                <div class="pr-5"><strong>999,999k</strong> followers</div>
+                <div class="pr-5"><strong>999,999k</strong> following</div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
+
+
+
+
